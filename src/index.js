be88cc00d71edda297a505d71d6d702c8d557b46
jacquibushfi <info@stainedglassofmckinney.com>
2020-02-29 04:48:58 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 // write your createStore function here
 
+function createStore(candyReducer) {
+  let state;
+  let store;
+
+  function dispatch(action) {
+    state = candyReducer(state, action);
+    store = state
+    render();
+  }
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState,
+    
+  };
+};
+
 function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
@@ -11,12 +32,14 @@ function candyReducer(state = [], action) {
 
 function render() {
   let container = document.getElementById('container');
-  if(store.getState()) {
+  if (store.getState()) {
     container.textContent = store.getState().join(' ')
   } else {
     throw new Error("the store's state has not been defined yet")
   }
 };
+
+dispatch({type: '@@INIT'})
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
